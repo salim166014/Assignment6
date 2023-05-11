eb556c3f8553a9c9e97de46173bfbb7a5d93f753
Salim Hossain Sagor <67732581+salim166014@users.noreply.github.com>
2023-05-11 22:23:52 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Array to store the books
+let books = [];
+
+// Route to serve the static index.html file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Route to get all books
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+// Route to add a book
+app.post('/books', (req, res) => {
+    const { title, author, publishedDate } = req.body;
+
+    // Generate a unique ID for the book
+    const id = Date.now().toString();
+
+    // Create the book object
+    const book = {
+        id,
+        title,
+        author,
+        publishedDate
+    };
+
+    // Add the book to the collection
+    books.push(book);
+
+    // Return the book object as the response
+    res.json(book);
+});
+
+// Route to delete a book by ID
+app.delete('/books/:id', (req, res) => {
+    const { id } = req.params;
+
+    // Find the index of the book with the specified ID
+    const bookIndex = books.findIndex(book => book.id === id);
+
+    // If the book exists, remove it from the collection
+    if (bookIndex !== -1) {
+        const deletedBook = books.splice(bookIndex, 1)[0];
+        res.json({ message: 'Book deleted successfully', book: deletedBook });
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// Sorry I don’t understand this assignment. So I Use Chat GPT.
+// I am sincerely sorry for such act.
+// Due to lack of time, I could not study on express. thank you. I hope, you see the apology in a good light.
